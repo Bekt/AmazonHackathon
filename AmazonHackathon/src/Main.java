40c726b7c376166130b494ea84031007eed919a4
unknown <gollapud@SEA-1201014898.ant.amazon.com>
2013-06-28 22:43:43 -0700

    Added code to check with previous picker lists

--- AmazonHackathon/src/Main.java
+++ AmazonHackathon/src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Main {
 	public static void main(String[] args) throws Exception {
@@ -5,56 +8,89 @@ public class Main {
 		Parse.readBinding();
 		Parse.readOrders();
 
+		List<Picker> pickerList = new ArrayList<Picker>();
 		PickerOperations ops = new PickerOperations();
 		Picker p = new Picker("picker-1");
-		int count = 1;
+		pickerList.add(p);
+		
+		System.out.println("Order A size: " + Parse.ordersA.size());
+		System.out.println("Order B size: " + Parse.ordersB.size());
+		System.out.println("Order C size: " + Parse.ordersC.size());
+		System.out.println("Order D size: " + Parse.ordersD.size());
+		
+		while (Parse.ordersD.size() > 0) {
+			if (!ops.getNextOrder(p, Parse.ordersD)) {
+				p = new Picker("picker-" + (pickerList.size() + 1));
+				System.out.println("New pickerList size: " + pickerList.size());
+				System.out.println("OrdersD size: " + Parse.ordersD.size());
+				pickerList.add(p);
+			}
+		}
+		
+		int totalCount = 0;
+		for(int i=0;i<pickerList.size();i++){
+			System.out.println(pickerList.get(i).toString());
+			totalCount += pickerList.get(i).completedOrders.size();
+		}
+		System.out.println("used " + pickerList.size() + " pickers");
+		System.out.println("Total: " + totalCount);
+		
+		System.exit(0);
 		
 		while (Parse.ordersA.size() > 0) {
 			if (!ops.getNextOrder(p, Parse.ordersA)) {
-				System.out.println(p.toString());
-				count++;
-				p = new Picker("picker-" + count);						
+				p = new Picker("picker-" + (pickerList.size() + 1));
+				pickerList.add(p);
 			}
 		}
-		System.out.println(p.toString());
 		
-		count++;
-		p = new Picker("picker-"+ count);
+		System.out.println("Completed A");
+		for(int i=0;i<pickerList.size();i++) {
+			p = pickerList.get(i);
+			while(ops.getNextOrder(p, Parse.ordersB)) {
+				
+			}
+		}
 		while (!Parse.ordersB.isEmpty()) {
 			if (!ops.getNextOrder(p, Parse.ordersB)) {
-				System.out.println(p.toString());
-				count++;
-				p = new Picker("picker-" + count);
+				p = new Picker("picker-" + (pickerList.size() + 1));
+				pickerList.add(p);
 			}
 		}
-		System.out.println(p.toString());
-		
-		count++;
-		p = new Picker("picker-"+ count);
+		System.out.println("Completed B");
+
+		for(int i=0;i<pickerList.size();i++) {
+			p = pickerList.get(i);
+			while(ops.getNextOrder(p, Parse.ordersC)) {
+			}
+		}
 		while (!Parse.ordersC.isEmpty()) {
 			if (!ops.getNextOrder(p, Parse.ordersC)) {
-				System.out.println(p.toString());
-				count++;
-				p = new Picker("picker-" + count);
-						
+				p = new Picker("picker-" + (pickerList.size() + 1));
+				pickerList.add(p);
 			}
 		}
-		System.out.println(p.toString());
-		
-		count++;
-		p = new Picker("picker-"+ count);
+		System.out.println("Completed C");
+		for(int i=0;i<pickerList.size();i++) {
+			p = pickerList.get(i);
+			while(ops.getNextOrder(p, Parse.ordersD)) {
+			}
+		}
 		while (!Parse.ordersD.isEmpty()) {
 			if (!ops.getNextOrder(p, Parse.ordersD)) {
-				System.out.println(p.toString());
-				count++;
-				p = new Picker("picker-" + count);
-						
+				p = new Picker("picker-" + (pickerList.size() + 1));
+				pickerList.add(p);
 			}
 		}
-		System.out.println(p.toString());
 		
-		System.out.println("used " + (count-1) + " pickers");
+		System.out.println("Completed D");
+		//int totalCount = 0;
+		for(int i=0;i<pickerList.size();i++){
+			System.out.println(pickerList.get(i).toString());
+			totalCount += pickerList.get(i).completedOrders.size();
+		}
+		System.out.println("used " + pickerList.size() + " pickers");
+		System.out.println("Total: " + totalCount);
 		Parse.in.close();
-	}
-	
+	}	
 }
