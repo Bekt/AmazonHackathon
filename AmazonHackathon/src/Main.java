9ef3b7498907ae449d273bff74ca0ec2b5846b08
bekt <bekt17@gmail.com>
2013-06-28 20:34:51 -0700

    1

--- AmazonHackathon/src/Main.java
+++ AmazonHackathon/src/Main.java
@@ -1,11 +1,11 @@
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		Parse.readInventory();
-		Parse.readBinding();
-		Parse.readOrders();
-
-		Parse.in.close();
+//		Parse.readInventory();
+//		Parse.readBinding();
+//		Parse.readOrders();
+//
+//		Parse.in.close();
 	}
 	
 }
